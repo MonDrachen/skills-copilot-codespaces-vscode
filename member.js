e893c5655bb0d26a270f52764235aac91fec1fef
Rafa Mondragón <111719734+MonDrachen@users.noreply.github.com>
2023-11-23 23:31:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var self = this;
+    self.id = '';
+    self.name = '';
+    self.email = '';
+    self.phone = '';
+    self.avatar = '';
+    self.level = '';
+    self.skills = [];
+    self.created_at = '';
+}
